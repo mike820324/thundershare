3a2d513248ddb76ebc0c28ae08edbd8d9eec1bc1
Raymon John <mike820324@gmail.com>
2024-02-01 17:17:34 +0800

    Add migration for filesharingmeta table

---
+++ thundershare-backend/migrations/20240201091625_Add_filesharingmeta_table.sql
@@ -0,0 +1,10 @@
+-- Add migration script here
+CREATE TABLE filesharingmeta (
+    id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    file_id UUID,
+    link TEXT,
+    expireat TIMETZ,
+    password TEXT,
+    FOREIGN KEY(file_id) REFERENCES file_meta(id)
+);
+
